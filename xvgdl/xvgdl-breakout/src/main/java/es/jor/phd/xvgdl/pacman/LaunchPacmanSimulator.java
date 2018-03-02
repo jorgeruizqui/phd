c0f78cb84f0e349c6109528f9e0fb60a27d4c340
Jorge Ruiz <jorge.ruiz@piksel.com>
2018-03-02 15:06:52 +0000

    Add XVGDL breakout

---
+++ xvgdl/xvgdl-breakout/src/main/java/es/jor/phd/xvgdl/pacman/LaunchPacmanSimulator.java
@@ -0,0 +1,21 @@
+package es.jor.phd.xvgdl.pacman;
+
+import es.jor.phd.xvgdl.pacman.genetic.PacmanGeneticAlg;
+
+/**
+ * Launcher
+ * @author jrquinones
+ *
+ */
+public class LaunchPacmanSimulator {
+
+    /**
+     * Main class
+     * @param args Arguments
+     */
+    public static void main(String[] args) {
+        PacmanGeneticAlg alg = new PacmanGeneticAlg(5, 100);
+        alg.startSimulation();
+    }
+
+}
---
+++ xvgdl/xvgdl-breakout/src/main/java/es/jor/phd/xvgdl/pacman/genetic/PacmanGeneticAlg.java
@@ -0,0 +1,277 @@
+package es.jor.phd.xvgdl.pacman.genetic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import es.indra.eplatform.util.log.ELogger;
+import es.jor.phd.xvgdl.app.GameApp;
+import es.jor.phd.xvgdl.context.GameContext;
+import es.jor.phd.xvgdl.engine.GameEngine;
+import es.jor.phd.xvgdl.model.endcondition.IGameEndCondition;
+import es.jor.phd.xvgdl.model.objectives.IGameObjective;
+import es.jor.phd.xvgdl.model.rules.GameRule;
+import es.jor.phd.xvgdl.model.rules.IGameRule;
+import es.jor.phd.xvgdl.pacman.context.PacmanSimulatorContextGenerator;
+import es.jor.phd.xvgdl.util.GameConstants;
+
+public class PacmanGeneticAlg {
+
+    private static final String GAME_SIMULATION_CATEGORY = "GAME_GENETIC_ALGORITHM_SIMULATION";
+
+	private static final int DEFAULT_POPULATION_NUMBER = 1;
+
+	private static final int DEFAULT_DISCARDED_POPULATION_NUMBER = 5;
+
+	private static final int DEFAULT_ITERATIONS = 100;
+
+	private List<GameContext> currentGameContexts = new ArrayList<>();
+    private GameEngine currentGameEngine;
+
+    private GameContext betterSolution;
+    private double currentBestSolution;
+
+    private int numberOfIterations;
+
+    /** Number of contexts actives for each generation. */
+    private int numberOfCurrentContext;
+    /** Number of discarded contexts for each generation. */
+    private int numberOfDiscardedContext;
+
+    public PacmanGeneticAlg(int numberOfIterations, int numberOfContext) {
+    	this(numberOfIterations, numberOfContext, DEFAULT_DISCARDED_POPULATION_NUMBER);
+    }
+
+    public PacmanGeneticAlg(int numberOfIterations) {
+    	this(numberOfIterations, DEFAULT_POPULATION_NUMBER, DEFAULT_DISCARDED_POPULATION_NUMBER);
+    }
+
+    public PacmanGeneticAlg() {
+    	this(DEFAULT_ITERATIONS, DEFAULT_POPULATION_NUMBER, DEFAULT_DISCARDED_POPULATION_NUMBER);
+    }
+
+    public PacmanGeneticAlg(int numberOfIterations, int numberOfContext, int numberOfDiscardedElements) {
+        this.numberOfIterations = numberOfIterations;
+        this.numberOfCurrentContext = numberOfContext;
+        this.numberOfDiscardedContext = numberOfDiscardedElements;
+    }
+
+    public void startSimulation() {
+
+    	long initTime = System.currentTimeMillis();
+
+    	// Generate base context from configuration
+    	// It will allow to generate NC context with a first evolution
+        loadBaseContexts();
+
+        // Force a first evolution of the contexts
+        this.currentGameContexts.forEach(this::evolution);
+
+        ELogger.info(this, GameConstants.GAME_CONTEXT_LOGGER_CATEGORY,
+        		"Starting simulation with generated base " + this.numberOfCurrentContext + " Game Contexts");
+
+        // Genetic algorithm main loop
+        int iterations = 0;
+        while (iterations++ < this.numberOfIterations) {
+            ELogger.debug(this, GAME_SIMULATION_CATEGORY, "Starting iteration " + iterations);
+            this.currentGameContexts.forEach(this::play);
+            // Order the results, bigger scores first
+            Collections.sort(this.currentGameContexts, Collections.reverseOrder());
+
+            // Discard worst population and generate new contexts
+            discardWorstContexts();
+            createNewContexts();
+
+            this.currentGameContexts.forEach(this::evolution);
+            this.currentGameContexts.forEach(this::mutation);
+        }
+
+        results();
+
+        ELogger.info(this, GameConstants.GAME_CONTEXT_LOGGER_CATEGORY,
+        		"End of simulation with DATA: TIME " + ((System.currentTimeMillis() - initTime) / 1000d) + " seconds." +
+         " ITERATIONS: " + this.numberOfIterations +
+         " POPULATION: " + this.numberOfCurrentContext);
+    }
+
+    /**
+     * Generate elements from the current list of context once the worst elements have been removed
+     */
+    private void createNewContexts() {
+    	int currentNumber = this.currentGameContexts.size();
+        PacmanSimulatorContextGenerator generator = new PacmanSimulatorContextGenerator();
+
+    	while (currentNumber++ < this.numberOfCurrentContext) {
+    		this.currentGameContexts.add(generator.generateContext(
+	                "/context/simul/pacmanSimulatorContextConfiguration.xml"));
+    	}
+	}
+
+	private void discardWorstContexts() {
+		int j = 0;
+		while (j++ < this.numberOfDiscardedContext) {
+			this.currentGameContexts.remove(this.currentGameContexts.size() - 1);
+		}
+	}
+
+	private void results() {
+        ELogger.info(this, "GAME_SIMULATION_RESULT",
+                "Simulation Ended after " + this.numberOfIterations + " executions. "
+                + "Fitness Score: " + this.currentBestSolution
+                + ". Game Context Solution: " + this.betterSolution);
+    }
+
+    private void mutation(GameContext c) {
+    	mutateMap(c);
+    }
+
+    /**
+     * Evolve current population
+     * @param c
+     */
+    private void evolution(GameContext c) {
+    	evolveMap(c);
+    	evolvePlayer(c);
+    	evolveEndConditions(c);
+    	evolveRules(c);
+    }
+
+    private void evolveEndConditions(GameContext c) {
+    	c.getEndConditions().stream().forEach(this::evolveEndCondition);
+    }
+
+    private void evolveEndCondition(IGameEndCondition endCondition) {
+		double generatedValue = ThreadLocalRandom.current().nextDouble(1.0d);
+    	if (generatedValue >= 0.95d) {
+    		endCondition.evolution();
+    	}
+    }
+
+    private void evolveRules(GameContext c) {
+    	c.getGameRules().stream().forEach(this::evolveRule);
+    }
+
+    private void evolveRule(IGameRule gameRule) {
+    	double generatedValue = ThreadLocalRandom.current().nextDouble(1.0d);
+    	if (generatedValue >= 0.95d) {
+    		gameRule.evolution();
+    	}
+    }
+
+    private void evolveMap(GameContext c) {
+    	int x = c.getGameMap().getSizeX();
+		double generatedValue = ThreadLocalRandom.current().nextDouble(1.0d);
+    	if (generatedValue >= 0.95d) {
+    		x++;
+    	} else if (generatedValue <= 0.05d) {
+    		x--;
+    	}
+
+		int y = c.getGameMap().getSizeY();
+		generatedValue = ThreadLocalRandom.current().nextDouble(1.1d);
+    	if (generatedValue >= 0.95d) {
+    		y++;
+    	} else if (generatedValue <= 0.05d) {
+    		y--;
+    	}
+
+    	int z = c.getGameMap().getSizeZ();
+		generatedValue = ThreadLocalRandom.current().nextDouble(1.1d);
+    	if (generatedValue >= 0.95d) {
+    		z++;
+    	} else if (generatedValue <= 0.05d) {
+    		z--;
+    	}
+
+    	c.getGameMap().resize(x, y, z);
+    }
+
+	private void evolvePlayer(GameContext c) {
+		double generatedValue = ThreadLocalRandom.current().nextDouble(1.1d);
+
+		int initialLives = c.getCurrentGamePlayer().getInitialLives();
+		if (generatedValue <= 0.05) {
+			initialLives--;
+		}
+		if (generatedValue >= 0.95) {
+			initialLives++;
+		}
+		c.getCurrentGamePlayer().setInitialLives(initialLives);
+	}
+
+    private void evaluate(GameContext c) {
+        double fitness = 0;
+
+        for (IGameObjective go : this.currentGameEngine.getGameContext().getGameObjectives()) {
+        	fitness += go.checkObjective(this.currentGameEngine.getGameContext());
+        }
+
+        ELogger.debug(this, GAME_SIMULATION_CATEGORY, "End of simulation with score" + fitness);
+        if (fitness > currentBestSolution) {
+            currentBestSolution = fitness;
+            betterSolution = c;
+            ELogger.debug(this, GAME_SIMULATION_CATEGORY, "New best score found for this context: " + betterSolution.toString());
+        }
+
+        // Set the fitness score to the Game Context
+        c.setFitnessScore(fitness);
+    }
+
+    private void play(GameContext c) {
+    	// Execute game simulation for this context
+        this.currentGameEngine = GameApp.launchGameApp(c, "/engine/simul/pacmanSimulatorEngineConfiguration.xml");
+        this.currentGameEngine.start();
+
+        // Evaluate the context and store its solution
+        evaluate(c);
+    }
+
+    private void loadBaseContexts() {
+        PacmanSimulatorContextGenerator generator = new PacmanSimulatorContextGenerator();
+
+        for (int i = 0; i < this.numberOfCurrentContext; i++){
+	        // Generating context from a base XML configuration
+	        this.currentGameContexts.add(generator.generateContext(
+	                "/context/simul/pacmanSimulatorContextConfiguration.xml"));
+        }
+
+    }
+
+    private void mutateMap(GameContext c) {
+    	int x = c.getGameMap().getSizeX();
+		double generatedValue = ThreadLocalRandom.current().nextDouble(1.0d);
+    	if (generatedValue >= 0.995d) {
+    		x+=20;
+    	} else if (generatedValue <= 0.005d) {
+    		x-=20;
+    	}
+
+		int y = c.getGameMap().getSizeY();
+		generatedValue = ThreadLocalRandom.current().nextDouble(1.1d);
+    	if (generatedValue >= 0.995d) {
+    		y+=20;
+    	} else if (generatedValue <= 0.005d) {
+    		y-=20;
+    	}
+
+    	int z = c.getGameMap().getSizeZ();
+		generatedValue = ThreadLocalRandom.current().nextDouble(1.1d);
+    	if (generatedValue >= 0.995d) {
+    		z+=20;
+    	} else if (generatedValue <= 0.005d) {
+    		z-=20;
+    	}
+
+    	c.getGameMap().resize(x, y, z);
+    }
+
+    private void mutateRules(GameContext c) {
+		double generatedValue = ThreadLocalRandom.current().nextDouble(1.0d);
+    	if (generatedValue >= 0.995d) {
+    		IGameRule rule = new GameRule();
+    		//rule.set
+			c.addRule(rule);
+    	}
+    }
+}
